08377d37baa6d6281130e62ffd41402e4acff25c
jasonChen1982 <982511509@qq.com>
2016-08-29 20:36:19 +0800

    增加一些方法

--- src/modules/animation.js
+++ src/modules/animation.js
@@ -75,49 +75,53 @@ Transition.prototype.cancle = function(){
 
 
 function Animation(opts){
-    JC.Transition.call(this);
+    opts.from = JC.copyJSON(opts.keys[0]);
+    opts.to = JC.copyJSON(opts.keys[1]);
+
+    JC.Transition.call(this,opts);
 
     this.keyframes = [];
-    this.keyIndex = 0;
+    this.keyIndex = 1;
+    this.direction = 1;
     this.keyFrames(opts);
+    this.element.setVal(this.ATRS);
 }
 JC.Animation = Animation;
 Animation.prototype = Object.create( JC.Transition.prototype );
 Animation.prototype.constructor = JC.Animation;
-Animation.prototype.update = function(){
+Animation.prototype.update = function(snippet){
     if(this.paused||!this.living)return;
-    var duration = this.keyframes[this.keyIndex].duration;
-    if(this.progress < duration){
+    this.progress += this.timeScale*snippet;
+
+    if(this.progress < this.duration){
         if(this.progress<0)return;
         var pose = this.nextPose();
-        this.onUpdate&&this.onUpdate(pose,this.progress/this.duration);
+        this.onUpdate&&this.onUpdate(pose,this.progress/this.duration,this.keyIndex);
     }else{
         this.element.setVal(this.ATRE);
-        this.onUpdate&&this.onUpdate(this.ATRE,1);
-        if(this.repeats>0||this.infinity){
-            this.repeats>0&&--this.repeats;
-            this.progress = 0;
-            if(this.alternate){
-                var sc = JC.copyJSON(this.ATRS);
-                this.ATRS = JC.copyJSON(this.ATRE);
-                this.ATRE = sc;
-            }else{
-                this.element.setVal(this.ATRS);
-            }
+        this.onUpdate&&this.onUpdate(this.ATRE,1,this.keyIndex);
+        if(this.keyIndex<this.keyframes.length-1&&this.keyIndex>0){
+            this.keyIndex += this.direction;
+            this.ATRS = this.keyframes[this.keyIndex].to;
+            this.duration = this.keyframes[this.keyIndex].duration||this.duration;
+            this.ease = this.keyframes[this.keyIndex].ease||this.ease;
         }else{
-            this.living = false;
-            this.onCompelete&&this.onCompelete();
+            if(this.repeats>0||this.infinity){
+                this.repeats>0&&--this.repeats;
+                this.progress = 0;
+                if(this.alternate){
+                    var sc = JC.copyJSON(this.ATRS);
+                    this.ATRS = JC.copyJSON(this.ATRE);
+                    this.ATRE = sc;
+                }else{
+                    this.element.setVal(this.ATRS);
+                }
+            }else{
+                this.living = false;
+                this.onCompelete&&this.onCompelete();
+            }
         }
     }
-    
-};
-Transition.prototype.nextPose = function(){
-    var cache = {};
-    for(var i in this.ATRE){
-        cache[i] = JC.TWEEN[this.ease]( this.progress , this.ATRS[i] , this.ATRE[i] - this.ATRS[i] , this.duration );
-        if(this.element[i]!==undefined)this.element[i] = cache[i];
-    }
-    return cache;
 };
 Animation.prototype.keyFrames = function(opts){
     for (var i = 0; i < opts.keys.length; i++) {
