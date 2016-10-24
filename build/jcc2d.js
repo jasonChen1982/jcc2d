befa04de215cfb3cd1de1ea2ca897b66ed5988a3
jason chen <cjx89807@alibaba-inc.com>
2016-10-24 11:46:40 +0800

    release new version

--- build/jcc2d.js
+++ build/jcc2d.js
@@ -1116,6 +1116,161 @@ Polygon.prototype.contains = function (x, y)
 };
 
 /**
+ * 圆形对象
+ *
+ * @class
+ * @memberof JC
+ * @param x {number} x轴的坐标
+ * @param y {number} y轴的坐标
+ * @param radius {number} 圆的半径
+ */
+function Circle(x, y, radius)
+{
+    /**
+     * @member {number}
+     * @default 0
+     */
+    this.x = x || 0;
+
+    /**
+     * @member {number}
+     * @default 0
+     */
+    this.y = y || 0;
+
+    /**
+     * @member {number}
+     * @default 0
+     */
+    this.radius = radius || 0;
+}
+
+/**
+ * 克隆一个该圆对象
+ *
+ * @return {PIXI.Circle} 克隆出来的圆对象
+ */
+Circle.prototype.clone = function ()
+{
+    return new Circle(this.x, this.y, this.radius);
+};
+
+/**
+ * 检测坐标点是否在园内
+ *
+ * @param x {number} 坐标点的x轴坐标
+ * @param y {number} 坐标点的y轴坐标
+ * @return {boolean} 坐标点是否在园内
+ */
+Circle.prototype.contains = function (x, y)
+{
+    if (this.radius <= 0)
+    {
+        return false;
+    }
+
+    var dx = (this.x - x),
+        dy = (this.y - y),
+        r2 = this.radius * this.radius;
+
+    dx *= dx;
+    dy *= dy;
+
+    return (dx + dy <= r2);
+};
+
+/**
+* 返回对象所占的矩形区域
+*
+* @return {PIXI.Rectangle} 矩形对象
+*/
+Circle.prototype.getBounds = function ()
+{
+    return new Rectangle(this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
+};
+
+/**
+ * 椭圆对象
+ *
+ * @class
+ * @memberof JC
+ * @param x {number} x轴的坐标
+ * @param y {number} y轴的坐标
+ * @param width {number} 椭圆的宽度
+ * @param height {number} 椭圆的高度
+ */
+function Ellipse(x, y, width, height)
+{
+    /**
+     * @member {number}
+     * @default 0
+     */
+    this.x = x || 0;
+
+    /**
+     * @member {number}
+     * @default 0
+     */
+    this.y = y || 0;
+
+    /**
+     * @member {number}
+     * @default 0
+     */
+    this.width = width || 0;
+
+    /**
+     * @member {number}
+     * @default 0
+     */
+    this.height = height || 0;
+}
+
+/**
+ * 克隆一个该椭圆对象
+ *
+ * @return {PIXI.Ellipse} 克隆出来的椭圆对象
+ */
+Ellipse.prototype.clone = function ()
+{
+    return new Ellipse(this.x, this.y, this.width, this.height);
+};
+
+/**
+ * 检测坐标点是否在椭园内
+ *
+ * @param x {number} 坐标点的x轴坐标
+ * @param y {number} 坐标点的y轴坐标
+ * @return {boolean} 坐标点是否在椭园内
+ */
+Ellipse.prototype.contains = function (x, y)
+{
+    if (this.width <= 0 || this.height <= 0)
+    {
+        return false;
+    }
+
+    //normalize the coords to an ellipse with center 0,0
+    var normx = ((x - this.x) / this.width),
+        normy = ((y - this.y) / this.height);
+
+    normx *= normx;
+    normy *= normy;
+
+    return (normx + normy <= 1);
+};
+
+/**
+ * 返回对象所占的矩形区域
+ *
+ * @return {PIXI.Rectangle} 矩形对象
+ */
+Ellipse.prototype.getBounds = function ()
+{
+    return new Rectangle(this.x - this.width, this.y - this.height, this.width, this.height);
+};
+
+/**
  * 矩阵对象，用来描述和记录对象的tansform 状态信息
  *
  * @class
@@ -2662,6 +2817,8 @@ exports.loaderUtil = loaderUtil;
 exports.Point = Point;
 exports.Rectangle = Rectangle;
 exports.Polygon = Polygon;
+exports.Circle = Circle;
+exports.Ellipse = Ellipse;
 exports.Matrix = Matrix;
 exports.IDENTITY = IDENTITY;
 exports.TEMP_MATRIX = TEMP_MATRIX;
