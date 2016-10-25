1b024cec53d0454aad500574f9ebec5c6095f49c
jason chen <cjx89807@alibaba-inc.com>
2016-10-25 21:24:46 +0800

    test: 修改测试代码

--- test/unit/display/Container.test.js
+++ test/unit/display/Container.test.js
@@ -1,12 +1,12 @@
 describe('JC.Container', function () {
     describe('parent', function () {
-        it('should be present when adding cds to Container', function() {
+        it('should be present when adding childs to Container', function() {
             var container = new JC.Container(),
                 child = new JC.DisplayObject();
 
-            expect(container.cds.length).to.be.equals(0);
-            container.addChilds(child);
-            expect(container.cds.length).to.be.equals(1);
+            expect(container.childs.length).to.be.equals(0);
+            container.adds(child);
+            expect(container.childs.length).to.be.equals(1);
             expect(child.parent).to.be.equals(container);
         });
     });
