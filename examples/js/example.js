0ede79cd3998efd03d54745070303c42ca4dabdc
jason chen <cjx89807@alibaba-inc.com>
2016-10-28 16:50:18 +0800

    examples: 终于找到了正确的打开方式

--- examples/js/example.js
+++ examples/js/example.js
@@ -3,21 +3,10 @@
 var list = '';
 for (var i = 0; i < EXAMPLES.length; i++) {
     var item = EXAMPLES[i];
-    list += '<li class="demo-item" data-url="'+item.link+'"><a href="demo_frames_sprite" target="viewPort">'+item.title+'</a></li>';
+    list += '<li class="demo-item"><a href="'+item.link+'/index.html" target="viewPort">'+item.title+'</a></li>';
 }
 
 $('#listBox').html(list);
-$('#listBox li').on('click',function(){
-    if (this.dataset.url) {
-        // document.getElementById('viewPort').src = location.href + this.dataset.url;
-        // var link = document.createElement( 'a' );
-        // link.className = 'link';
-        // link.textContent = name;
-        // link.href = location.href + this.dataset.url;
-        // link.setAttribute( 'target', 'viewPort' );
-        // link.click();
-    }
-});
 
 $('#sideBox').on('mouseover',function() {
     $('#windowBox').addClass('active-window');
