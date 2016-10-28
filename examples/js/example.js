69bf7f0ee793bb32aaf3a63a3bf60b7f4df5e59e
jason chen <cjx89807@alibaba-inc.com>
2016-10-28 16:33:09 +0800

    examples: 呵呵

--- examples/js/example.js
+++ examples/js/example.js
@@ -3,34 +3,34 @@
 var list = '';
 for (var i = 0; i < EXAMPLES.length; i++) {
     var item = EXAMPLES[i];
-    list += '<li class="demo-item" data-url="'+item.link+'">'+item.title+'</li>';
+    list += '<li class="demo-item" data-url="'+item.link+'"><a href="demo_frames_sprite" target="viewPort">'+item.title+'</a></li>';
 }
 
 $('#listBox').html(list);
 $('#listBox li').on('click',function(){
     if (this.dataset.url) {
         // document.getElementById('viewPort').src = location.href + this.dataset.url;
-        var link = document.createElement( 'a' );
+        // var link = document.createElement( 'a' );
         // link.className = 'link';
         // link.textContent = name;
-        link.href = location.href + this.dataset.url;
-        link.setAttribute( 'target', 'viewPort' );
-        link.click();
+        // link.href = location.href + this.dataset.url;
+        // link.setAttribute( 'target', 'viewPort' );
+        // link.click();
     }
 });
 
 $('#sideBox').on('mouseover',function() {
-    $(document.body).addClass('active-window');
+    $('#windowBox').addClass('active-window');
 });
 $('#sideBox').on('mouseout',function() {
-    $(document.body).removeClass('active-window');
+    $('#windowBox').removeClass('active-window');
 });
 
 $('#sideBox').on('click',function() {
-    $(document.body).addClass('active-window');
+    $('#windowBox').addClass('active-window');
 });
 $('#center-view-box').on('click',function() {
-    $(document.body).removeClass('active-window');
+    $('#windowBox').removeClass('active-window');
 });
 
 stroll.bind( $( '.demo-list-box' ) );
