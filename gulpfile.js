413bf19ae7b9e248c46628caf187071121d42f99
jason chen <cjx89807@alibaba-inc.com>
2016-11-29 17:53:20 +0800

    fix: fix error eslint for .js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp'),
-  path = require('path'),
-  uglify = require('gulp-uglify'),
-  sourcemaps = require('gulp-sourcemaps'),
-  rename = require('gulp-rename'),
-  pkg = require('./package.json');
+    path = require('path'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rename = require('gulp-rename'),
+    pkg = require('./package.json');
 
 
 
 gulp.task('release', function() {
-  return gulp.src('build/jcc2d.js')
+    return gulp.src('build/jcc2d.js')
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.init())
     .pipe(uglify())
