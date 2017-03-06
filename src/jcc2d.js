7831a393a99327aa3a600e97459f6a8d2d0b65e9
jason chen <cjx89807@alibaba-inc.com>
2017-03-06 20:55:46 +0800

    fix: adjust modules mapping

--- src/jcc2d.js
+++ src/jcc2d.js
@@ -20,7 +20,7 @@ export { DisplayObject } from './modules/core/display/DisplayObject';
 export { Container } from './modules/core/display/Container';
 export { Sprite } from './modules/core/display/Sprite';
 export { Graphics } from './modules/core/display/Graphics';
-export { Text } from './modules/core/display/Text';
+export { TextFace } from './modules/core/display/TextFace';
 export { BlurFilter } from './modules/filters/BlurFilter';
 
 export { Stage } from './modules/core/Stage';
