83addaf5c1c21862ed21179110f6a0251205873a
jason chen <cjx89807@alibaba-inc.com>
2017-05-26 21:44:36 +0800

    fix(container): fix sort fn

--- src/core/Container.js
+++ src/core/Container.js
@@ -140,7 +140,7 @@ Container.prototype.adds = function(object) {
     }
     object.parent = this;
     this.childs.push(object);
-    this.souldSort = true;
+    if (object.zIndex !== 0) this.souldSort = true;
   } else {
     console.error('adds: object not an instance of Container', object);
   }
