5b81ea60263572c643d040a39748a7aa25d97c89
jasonChen1982 <jasonchen1882@gmail.com>
2020-07-24 11:35:34 +0800

    feat(build): upgrade build script

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+
+import babel from 'rollup-plugin-babel';
+import {uglify} from 'rollup-plugin-uglify';
+
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      format: 'umd',
+      name: 'JC',
+      extend: true,
+      sourcemap: true,
+      file: 'build/jcc2d.js',
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+        presets: [['@babel/preset-env', {modules: false}]],
+        plugins: ['@babel/plugin-external-helpers', '@babel/plugin-proposal-object-rest-spread'],
+      }),
+      // uglify(),
+    ],
+    watch: {
+      exclude: ['node_modules/**'],
+    },
+  },
+  // {
+  //   input: 'src/index.js',
+  //   output: {
+  //     format: 'umd',
+  //     name: 'JC',
+  //     extend: true,
+  //     sourcemap: true,
+  //     file: 'build/jcc2d.js',
+  //   },
+  //   plugins: [
+  //     babel({
+  //       exclude: 'node_modules/**',
+  //       presets: [['@babel/preset-env', {modules: false}]],
+  //       plugins: ['@babel/plugin-external-helpers', '@babel/plugin-proposal-object-rest-spread'],
+  //     }),
+  //     uglify(),
+  //   ],
+  //   watch: {
+  //     exclude: ['node_modules/**'],
+  //   },
+  // },
+];
