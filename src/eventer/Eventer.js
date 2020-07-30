aa61baa6528b10f00c1d05217446aec84a335802
jasonChen1982 <jasonchen1882@gmail.com>
2020-07-30 19:24:57 +0800

    fix(interactive): jcc2d interactive should true

--- src/eventer/Eventer.js
+++ src/eventer/Eventer.js
@@ -1,1 +1,17 @@
-export {Eventer} from '../lottie-core/index';
+import {Eventer, Tools} from '../lottie-core/index';
+
+
+/**
+ * 事件对象的事件绑定函数
+ *
+ * @param {String} type 事件类型
+ * @param {Function} fn 回调函数
+ * @return {this}
+ */
+Eventer.prototype.on = function(type, fn) {
+  if (!Tools.isFunction(fn)) return this;
+  if (Tools.isUndefined(this.listeners[type])) this.listeners[type] = [];
+  this.listeners[type].push(fn);
+  this.interactive = true;
+  return this;
+};
